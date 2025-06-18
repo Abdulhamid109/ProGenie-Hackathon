bf77345aa9d26a28e2dcbd76d26553fce3b7d4d9
Abdulhamid109 <abdulhamidpatel109@gmail.com>
2025-06-18 22:35:30 +0530

    4th change

--- src/app/user/pages/aboutus/page.tsx
+++ src/app/user/pages/aboutus/page.tsx
@@ -4,10 +4,10 @@ export default function AboutUs() {
   return (
     <div className="flex-1 flex flex-col min-h-screen bg-gray-100">      
         
-      {/* Main Content */}
+      
       <main className="flex-1 p-6 w-full">
         <div className="max-w-7xl mx-auto">
-          {/* Hero Section */}
+          
           <section className="text-center py-12">
             <h1 className="text-4xl font-bold text-gray-800 mb-4">
               About littejob
