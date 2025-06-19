c5f463f997e2cb47b52b5cf4a94bd59dd22f0259
Abdulhamid109 <abdulhamidpatel109@gmail.com>
2025-06-20 00:18:30 +0530

    new update

--- src/app/user/pages/jobdescription/page.tsx
+++ src/app/user/pages/jobdescription/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const JobdescriptionPage = () => {
   return (
-    <div>JobdescriptionPage user</div>
+    <div>
+      <div className='bg-zinc-700 p-3 rounded-md bg-gradient-to-l from-black/[0.96] mb-2'>
+        <h1 className='font-bold text-2xl text-shadow-2xs p-2'>Job title</h1>
+        <h1 className='font-light text-lg p-2'>Experience</h1>
+        <h1 className='font-light text-lg p-2'>job Role</h1>
+        <h1 className='font-light text-lg p-2'>Salary Range</h1>
+        <h1 className='font-light text-sm p-2'>Skills</h1>
+      </div>
+      <div className='bg-zinc-700 p-3 rounded-md bg-gradient-to-l from-black/[0.96] mb-2'>
+        <div className='font-bold text-2xl text-shadow-2xs p-2'>Job Description</div>
+        <div className='font-light text-lg tracking-tight p-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro reprehenderit numquam fuga amet. Dolores, assumenda! Maiores, consectetur vero officia doloribus nisi mollitia tenetur blanditiis perspiciatis cumque rem nam dolorum itaque adipisci perferendis qui veniam earum non, ipsa accusantium, quibusdam quam? Porro explicabo iste nulla, reprehenderit qui quaerat iure eius inventore nesciunt accusantium perferendis architecto beatae quia expedita velit id itaque pariatur ratione labore, recusandae molestias accusamus quam dolores. Earum, dolor ipsum porro unde temporibus repudiandae commodi asperiores laboriosam tenetur dignissimos dolores maiores eaque, doloremque praesentium cumque! Saepe sed necessitatibus accusantium iste voluptatum! Eveniet ex provident error soluta iure molestias, vel commodi odio, et ut voluptatem obcaecati repellat. Nam, commodi ex earum facilis excepturi sed dolorum itaque animi sequi accusantium, iusto maiores ducimus! Reprehenderit distinctio doloribus minus cum ipsum consectetur animi voluptatem rerum hic nulla excepturi minima, fugiat alias repellat impedit quo sequi quisquam qui, molestiae ab ipsa labore nostrum! Autem quidem quia porro tenetur cumque ea ab facilis optio laborum mollitia delectus quis id maiores, culpa quas illum similique cum itaque, consequuntur sed commodi unde eligendi rem minima! Sapiente hic praesentium nesciunt et officia, dolorem facere. Debitis, sunt odio excepturi qui dicta quidem enim, nemo, est hic voluptatibus labore molestiae.</div>
+      </div>
+      <div className='bg-zinc-700 p-3 rounded-md bg-gradient-to-l from-black/[0.96] '>
+        <div className='font-bold text-2xl text-shadow-2xs p-2'>
+          About Company
+        </div>
+        <div className='font-light text-lg tracking-tight p-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, doloremque! Quam, nemo? Aperiam veniam, consequatur voluptatem, necessitatibus aliquid sint ducimus qui reiciendis nemo accusantium officiis quas dicta molestiae consequuntur cum odio asperiores a magnam distinctio. Vero reiciendis aliquam dolores quia repellendus velit officiis nisi facilis, magnam temporibus sapiente id animi quod obcaecati odit blanditiis odio quas architecto est possimus! Dicta odio magni eveniet ad cupiditate nulla culpa quos, doloremque suscipit dolore fugit? Laboriosam in quis ipsa libero rem, assumenda quaerat sequi explicabo eaque similique sunt magni quas distinctio suscipit at molestias. Unde delectus debitis rem eaque adipisci, possimus iure ex.</div>
+        </div>
+    </div>
   )
 }
 
